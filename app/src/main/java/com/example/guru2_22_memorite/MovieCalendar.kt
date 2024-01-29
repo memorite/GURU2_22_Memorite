bded93525a66752bb7025b8e9b4ea051e935edf2
cchhooina <cchhooina@gmail.com>
2024-01-29 15:20:20 +0900

    캘린더 기능 추가

--- app/src/main/java/com/example/guru2_22_memorite/MovieCalendar.kt
+++ app/src/main/java/com/example/guru2_22_memorite/MovieCalendar.kt
@@ -4,69 +4,45 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
-import android.widget.Toast
+import android.widget.CalendarView
+import java.text.SimpleDateFormat
+import java.time.Month
+import java.util.Calendar
+import java.util.Locale
 
 class MovieCalendar : AppCompatActivity() {
-
-    lateinit var SelectButton: Button
-    private var doubleBackToExit = false        // 뒤로가기로 인한 자동 앱 종료 방지용
+    private lateinit var dbManager: MovieActivity
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_calendar)
 
-        // 선택 버튼
-        SelectButton = findViewById<Button>(R.id.SelectButton)
+        dbManager = MovieActivity()
 
-        SelectButton.setOnClickListener {
+        val calendarView: CalendarView = findViewById(R.id.CalendarView)
+
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val date = getDate(year, month, dayOfMonth)
+            val content = getLogContent(date)
             val intent = Intent(this, MovieActivity::class.java)
+            intent.putExtra("date", date)
+            intent.putExtra("content", content)
             startActivity(intent)
         }
     }
 
-    // 목록으로 이동(책, 드라마)
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.calendar_movie_menu, menu)
-        return true
+    private fun getDate(year: Int, month: Int, dayOfMonth: Int): String {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, dayOfMonth)
+        val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        return sdf.format(calendar.time)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item?.itemId) {
-            R.id.action_book -> {
-                val intent = Intent (this, BookActivity::class.java)
-                startActivity(intent)
-                return true
-            }
-            R.id.action_drama -> {
-                val intent = Intent (this, DramaActivity::class.java)
-                startActivity(intent)
-                return true
-            }
-
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    // 뒤로가기를 더블클릭 했을 때만 앱이 종료되도록 설정
-    override fun onBackPressed() {
-        if(doubleBackToExit){
-            finishAffinity()
-        } else{
-            Toast.makeText(this, "종료하시려면 뒤로가기를 한번 더 눌러주세요.", Toast.LENGTH_SHORT).show()
-            doubleBackToExit = true
-            runDelayed(1500L){
-                doubleBackToExit = false
-            }
-        }
-    }
-
-    private fun runDelayed(millis: Long, function: () -> Unit) {
-        Handler(Looper.getMainLooper()).postDelayed(function, millis)
+    private fun getLogContent(date: String): String {
+        return "기록"
     }
 }
